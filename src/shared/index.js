7aa120dad0d1af7d4c00680d6c28b1c1d1f923d0
manishdashsharma <mdashsharma95@gmail.com>
2026-04-21 18:08:07 +0530

    feat: initialize backend project structure with database, authentication, logging, and health monitoring modules

---
+++ src/shared/index.js
@@ -0,0 +1,29 @@
+// Constants
+export { EApplicationEnvironment } from './constant/application.js';
+
+// Middleware
+export {
+  errorHandler,
+  notFoundHandler,
+  asyncHandler,
+  AppError,
+  createError,
+} from './middleware/errorHandler.js';
+// export { authenticate, isAdmin } from './middleware/auth.middleware.js';
+export { validateRequest } from './middleware/validate.middleware.js';
+
+// Utils
+export { default as logger } from './utils/logger.js';
+export {
+  httpResponse,
+  httpError,
+  errorObject,
+  responseMessage,
+} from './utils/response.js';
+export {
+  generateTokens,
+  verifyAccessToken,
+  verifyRefreshToken,
+  setCookieOptions,
+} from './utils/jwt.js';
+export { hashPassword, comparePassword } from './utils/password.js';
