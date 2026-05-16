3babf6e4abe45f936c52db70aa0efde1c1e976c2
manishdashsharma <mdashsharma95@gmail.com>
2026-05-16 13:44:55 +0530

    feat(email): add email preview templates for various notifications

--- check_admin.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-mongoose.connect(process.env.MONGODB_URL).then(async () => {
-  const users = await mongoose.connection.collection('users').find({ role: 'admin' }).toArray();
-  console.log(JSON.stringify(users, null, 2));
-  process.exit(0);
-});
